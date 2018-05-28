59764598d307a2cab94689a8bf0ccfda37944804
maikelmatamoros <maikelmatamorosz@gmail.com>
2018-05-28 15:28:48 -0600

    cosa

--- data/PersonData.java
+++ data/PersonData.java
@@ -10,6 +10,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import org.jdom.Document;
 import org.jdom.Element;
@@ -218,5 +219,30 @@ public class PersonData {
         }
         return message;
     }
-
+        private ArrayList<ArrayList<Element>> list=new ArrayList<>();
+        
+ 
+    public boolean search(Element hijo,int nivel) {
+        if (hijo.getChildren().isEmpty()) {
+            return true;
+        } else {
+            System.err.println("Entra");
+            List allElements = hijo.getChildren();
+            for (Object objectActual : allElements) {
+                Element elementoActual = (Element) objectActual;
+                list.get(nivel).add(elementoActual);
+                search(elementoActual, nivel+1);
+            }
+        }
+        return false;
+    }
+    
+    public ArrayList<ArrayList<Element>> retorno(){
+        list.add(new ArrayList<>());
+        list.add(new ArrayList<>());
+        list.add(new ArrayList<>());
+        list.add(new ArrayList<>());
+        search(root, 0);
+        return list;
+    }
 } // end class
--- tarea7yerlinleal/Tarea7YerlinLeal.java
+++ tarea7yerlinleal/Tarea7YerlinLeal.java
@@ -3,14 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tarea7yerlinleal;
 
 import data.PersonData;
 import domain.Person;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.jdom.Element;
 import org.jdom.JDOMException;
 
 /**
@@ -25,9 +26,13 @@ public class Tarea7YerlinLeal {
     public static void main(String[] args) {
         try {
             PersonData data = new PersonData();
-            data.insertPerson1(new Person("56789", "PEPE", "Lopéz", "Caza", "14/06/97", "Costa Rica", "111"));
-            
-            
+            data.insertPerson1(new Person("111213", "PEPE", "Lopéz", "Caza", "14/06/97", "Costa Rica", "56789"));
+//            data.insertPerson1(new Person("56782", "PEPE", "Lopéz", "Caza", "14/06/97", "Costa Rica", "113"));
+//            data.insertPerson1(new Person("56784", "PEPE", "Lopéz", "Caza", "14/06/97", "Costa Rica", "112"));
+//            data.insertPerson1(new Person("56785", "PEPE", "Lopéz", "Caza", "14/06/97", "Costa Rica", "115"));
+            ArrayList<ArrayList<Element>> lista = data.retorno();
+            System.err.println(lista.get(1).size());
+
 //            Person p[] = data.getAllPeople();
 //            for (Person person : p) {
 //                System.out.println(person.toString());
@@ -35,9 +40,7 @@ public class Tarea7YerlinLeal {
         } catch (JDOMException | IOException ex) {
             Logger.getLogger(Tarea7YerlinLeal.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        
+
     }
 
 }
