6678f874d9c2bdc836475dea9cbf74a8a4df7be6
YerLeal <yeryazmin96@gmail.com>
2018-05-30 20:13:27 -0600

    Commit final

--- tarea7yerlinleal/Tarea7YerlinLeal.java
+++ tarea7yerlinleal/Tarea7YerlinLeal.java
@@ -10,7 +10,6 @@ import domain.Person;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jdom.Element;
 import org.jdom.JDOMException;
 
 /**
@@ -23,7 +22,12 @@ public class Tarea7YerlinLeal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        try {
+            PersonData data = new PersonData();
+//            data.insertPerson(new Person(id, firstName, surname1, surname2, birthdate, country, fatherId));
+        } catch (JDOMException | IOException ex) {
+            Logger.getLogger(Tarea7YerlinLeal.class.getName()).log(Level.SEVERE, null, ex);
+        } 
     } // main
 
 } // end class
